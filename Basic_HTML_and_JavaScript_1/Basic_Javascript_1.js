878229b8a1e8f812a9e70d4429c4e1d7ea95edd4
Steve Thornton <technission@outlook.com>
2023-08-01 14:05:51 -0500

    This is the BASIC HTML AND JAVASCRIPT 1 ASSIGNMENT

---
+++ Basic_HTML_and_JavaScript_1/Basic_Javascript_1.js
@@ -0,0 +1,7 @@
+alert("This is proof that I know what I am doing!")
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+}
